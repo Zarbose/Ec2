d3663a8769d96d68c036f8b96f14a568395c1d86
Zarbose <simonpieto.pieto@gmail.com>
2023-04-30 21:24:50 +0200

    bug de maj du code

--- webapp/Ec2/scrap/urls.py
+++ webapp/Ec2/scrap/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import index
-from .views import test
-from .views import get_name
+from scrap.views import index
+from scrap.views import grafana
+from scrap.views import get_name
+from scrap.views import scenario_delete
 
 urlpatterns = [
     path('', index, name="scrap-index"),
-    path('test/', test, name="scrap-test"),
+    path('grafana/', grafana, name="scrap-grafana"),
     path('get_name/', get_name, name="scrap-test"),
+    path('scenario_delete/<int:id>/', scenario_delete, name="scrap-delete"),
 ]
--- webapp/Ec2/scrap/views.py
+++ webapp/Ec2/scrap/views.py
@@ -1,24 +1,53 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
 from scrap.forms import ScenarioForm
 from scrap.models import Scenario
 
 def index(request):
     return render(request,"scrap/index.html")
 
-def test(request):
+def grafana(request):
     scenarios = Scenario.objects.all()
     context = {
         "scenarios": scenarios
     }
     return render(request,"scrap/test.html",context)
 
+def scenario_delete(request, id):
+    scenario = Scenario.objects.get(id=id)
+    scenario.delete()
+
+    return HttpResponseRedirect("/get_name/")
+
+def scenario_modify(request, id):
+    scenario = Scenario.objects.get(id=id)
+    scenarios = Scenario.objects.all()
+    context = {
+        "form": ScenarioForm(),
+        "cur_scenario": scenario,
+        "scenarios": scenarios,
+        "scenarios_lenght": scenarios.count()
+        # "scenarios_lenght": 0
+    }
+
+    return render(request, "scrap/name.html", context)
+
 def get_name(request):
+
+    # if 'small_simuler' in request.POST:
+    #     return HttpResponseRedirect("/grafana/")
+    
+    # elif 'small_modifier' in request.POST:
+    #     id = request.POST['small_modifier']
+    #     print("Modification ",request.POST['small_modifier'])
+
+    # elif 'small_suprimer' in request.POST:
+    #     id = request.POST['small_suprimer']
+    #     return HttpResponseRedirect("/scenario_delete/"+id)
 
     if request.method == "POST":
         form = ScenarioForm(request.POST)
-
+        print(request.POST)
         if form.is_valid():
 
             scenario = Scenario()
