5116729c716683c23bea5d7306812e7b565916bb
Zarbose <simonpieto.pieto@gmail.com>
2023-05-08 00:43:18 +0200

    construction liste segment

--- wip/scrapV2.py
+++ wip/scrapV2.py
@@ -1,5 +1,6 @@
 import requester as rq
 import utilsV2 as ut
+from datetime import timedelta
 
 def scrap_initDailyPrice():
     result=[]
@@ -34,21 +35,88 @@ def scrap_extract_params(params):
 
     return formatted_settings
 
+def scrap_contruct_segment_list(formatted_prices,formatted_settings):
+    asc_duration = ut.utils_get_duration_activation(formatted_settings['target'],formatted_settings['asc_consomation'])
+    asc_duration_int=int(asc_duration)
+    asc_duration_deci=asc_duration % 1
+
+    # print(asc_duration_int,asc_duration_deci)
+
+    # Construction de la liste des segments ou le pompage va être effectué
+    if asc_duration_deci != 0:
+        nb_segments = asc_duration_int+1
+    else:
+        nb_segments = asc_duration_int
+
+    list_segments = []
+    for i in range(nb_segments):
+        list_segments.append(formatted_prices[i])
+    
+    list_segments.sort(key=ut.utils_key_sorted_times)
+
+    # ut.utils_print_prices(list_segments)
+
+    # Calcul de la date de fin
+    total_sec=asc_duration*3_600
+    end_sec=3_600-(total_sec-3_600*asc_duration_int)
+    # end_sec=int(end_sec/60)
+
+    end=list_segments[len(list_segments)-1]
+    end = (end['time'] + timedelta(hours=1)) - timedelta(seconds=end_sec)
+    # print(end)
+
+    return {"segments":list_segments, "end":end}
+
+def scrap_construct_influxdb_list(optimized_segment_list):
+    end = optimized_segment_list['end']
+    list_segments = optimized_segment_list['segments']
+
+    points = []
+    old_point_end=list_segments[0]['time'] + timedelta(hours=1)
+
+    for elm in list_segments:
+        point_to_add =  {'start':elm['time'] ,'end':elm['time'] + timedelta(hours=1)}
+
+        if len(points) == 0:
+            old_point_end=list_segments[0]['time'] + timedelta(hours=1)
+            points.append({'start':list_segments[0]['time'] ,'end':list_segments[0]['time'] + timedelta(hours=1)})
+        elif point_to_add['start'] == old_point_end: # Contigue
+            points[len(points)-1]['end'] = point_to_add['end']
+        else: # Non contigue
+            points.append(point_to_add)
+
+        old_point_end=point_to_add['end']
+
+    points[len(points)-1]['end'] = end
+
+    return points
+
+def scrap_optimisation(formatted_prices,formatted_settings):
+    optimized_segment_list = scrap_contruct_segment_list(formatted_prices,formatted_settings)
+    point_list = scrap_construct_influxdb_list(optimized_segment_list)
+    for elm in ut.utils_format_point_to_influxdb(point_list):
+        print(elm)
+
+
+
 if __name__ == "__main__":
 
-    input_params = {'asc_consomation': '12', 'asc_consomation_choices': 1, 
-                    'asc_tmp_min': '12', 'asc_tmp_min_choices': 1, 
-                    'asc_capa_max': '12', 'asc_capa_max_choices': 1, 
-                    'asc_capa_actu': '12', 'asc_capa_actu_choices': 1, 
-                    'desc_consomation': '12', 'desc_consomation_choices': 1, 
-                    'desc_capa_max': '12', 'desc_capa_max_choices': 1, 
-                    'desc_capa_actu': '12', 'desc_capa_actu_choices': 1, 
-                    'target': '12', 'target_choices': 1, 
+    input_params = {'asc_consomation': '1_270', 'asc_consomation_choices': 4, 
+                    'asc_tmp_min': '1', 'asc_tmp_min_choices': 1, 
+                    'asc_capa_max': '37_000', 'asc_capa_max_choices': 4, 
+                    'asc_capa_actu': '20_000', 'asc_capa_actu_choices': 4, 
+                    'desc_consomation': '1_800', 'desc_consomation_choices': 4, 
+                    'desc_capa_max': '33_000', 'desc_capa_max_choices': 4, 
+                    'desc_capa_actu': '3_000', 'desc_capa_actu_choices': 4, 
+                    'target': '5_000', 'target_choices': 4, 
                     'titre': 'Titre'}
     
     prices = scrap_extract_prices()
-    prices = scrap_format_prices(prices)
+    # ut.utils_print_prices(prices)
+    formatted_prices = scrap_format_prices(prices)
     formatted_settings = scrap_extract_params(input_params)
 
-    ut.utils_print_prices(prices)
-    print(formatted_settings)
+    # ut.utils_print_prices(prices)
+    # ut.utils_print_params(formatted_settings)
+
+    scrap_optimisation(formatted_prices,formatted_settings)
--- wip/utilsV2.py
+++ wip/utilsV2.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from datetime import timedelta
 
 WATT_CHOICES ={'1': "W",'2': "KW",'3': "MW",'4': "GW",'5': "TW"}
 
@@ -6,8 +7,15 @@ WATTH_CHOICES = {'1': "Wh",'2': "KWh",'3': "MWh",'4': "GWh",'5': "TWh"}
 
 TIME_CHOICES ={'1': "H",'2': "M",'3': "S"}
 
+# Key function to sort list
 def utils_key_sorted_prices(prices):
     return float(prices['val'])
+
+def utils_key_sorted_times(list):
+    return list['time']
+
+def utils_key_sorted_influxdb(list):
+    return str(list['time'])
 
 def utils_format_watt(elm,unit):
     elm=float(elm)
@@ -24,7 +32,6 @@ def utils_format_watt(elm,unit):
     elif (unit == 5):
         return elm*1_000_000_000_000
 
-
 def utils_format_time(elm,unit):
     elm=float(elm)
     unit=int(unit)
@@ -35,13 +42,6 @@ def utils_format_time(elm,unit):
         return elm*60
     elif (unit == 3):
         return elm
-    
-# 2023-05-07T14:00:00Z ==> datetime(2023-05-07T14:00:00)
-def utils_format_infuxsb_to_datetime(elm):
-    date=elm
-    date=date.replace("Z","")
-    date = datetime.strptime(date,'%Y-%m-%dT%H:%M:%S')
-    return date
 
 # 2023-05-07T14:00:00Z ==> datetime(14:00:00)
 def utils_format_infludb_to_datetime(elm):
@@ -52,6 +52,50 @@ def utils_format_infludb_to_datetime(elm):
     date = datetime.strptime(date,'%H:%M:%S')
     return (date)
 
+def utils_format_datetime_to_infuxdb(elm):
+    elm = elm.replace(microsecond=0)
+    curent_day = datetime.today().strftime('%Y-%m-%d')
+    string=str(curent_day)+"T"+str(elm.time())+"Z"
+    return string
+
+def utils_format_point_to_influxdb(point_list): # bugger
+    formated_list=[]
+    for elm in point_list:
+        formated_list.append({ 'time': utils_format_datetime_to_infuxdb(elm['start']), 'val': 1})
+        formated_list.append({ 'time': utils_format_datetime_to_infuxdb(elm['end']), 'val': 1})
+
+    string = "1900 1 1 0 0"
+    old_time = datetime.strptime(string, "%Y %m %d %H %M")
+
+    copy_formated_list = formated_list
+
+    for elm in copy_formated_list:
+        while old_time < utils_format_infludb_to_datetime(elm['time']):
+            if elm == formated_list[len(formated_list)-1]:
+                formated_list.append({ 'time': utils_format_datetime_to_infuxdb(old_time), 'val': 0})
+                formated_list.append({ 'time': utils_format_datetime_to_infuxdb(old_time + timedelta(hours=1)), 'val': 0})
+            else:
+                formated_list.append({ 'time': utils_format_datetime_to_infuxdb(old_time), 'val': 0})
+            old_time += timedelta(hours=1)
+        old_time += timedelta(hours=1)
+
+    while old_time < datetime.strptime("1900 1 1 23 59", "%Y %m %d %H %M"):
+        formated_list.append({ 'time': utils_format_datetime_to_infuxdb(old_time), 'val': 0})
+        old_time += timedelta(hours=1)
+
+    formated_list.sort(key=utils_key_sorted_influxdb)
+    return formated_list
+
+def utils_get_duration_activation(target,energy):
+    a = energy # Watt/h
+    b = target # Watt
+    return b/a
+
+# Print functions
 def utils_print_prices(prices):
     for elm in prices:
         print(elm['time'],elm['val'])
+
+def utils_print_params(params):
+    for keys,value in params.items():
+        print(keys,value)
